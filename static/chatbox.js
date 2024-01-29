a792c371530bbc25a44ace68987641ceee441e7f
Turgut Muslu <c.tr.muslu@gmail.com>
2024-01-29 23:24:35 +0300

    son

--- static/chatbox.js
+++ static/chatbox.js
@@ -26,38 +26,27 @@ function sendMessage() {
 
 document.addEventListener('DOMContentLoaded', function () {
   // İlk giriş mesajlarını simüle etme
-  addMessage('Merhaba! Chatbox\'a hoş geldiniz.', 'incoming');
+  addMessage('Merhaba afacan! Rivehold\'a hoş geldin!', 'incoming');
 });
 
 // Arka plan resmini değiştirmek için JavaScript
-const backgroundImages = ['image/sph3.jpg', 'image/sph1.jpg', 'image/sph2.jpg', 'image/sph4.jpg', 'image/sph5.jpg'];
+const backgroundImages = ['static/sph3.jpg', 'static/sph1.jpg', 'static/sph2.jpg', 'static/sph4.jpg', 'static/sph5.jpg'];
 let currentImageIndex = 0;
 
 function changeBackgroundImage() {
-  document.body.style.backgroundImage = `url('${backgroundImages[currentImageIndex]}')`;
+  document.body.style.backgroundImage = 'url(' + backgroundImages[currentImageIndex] + ')';
   currentImageIndex = (currentImageIndex + 1) % backgroundImages.length;
 }
 
+
 // Sayfa yüklendiğinde ve belirli bir süre sonra arkaplan fotoğrafını değiştir
 document.addEventListener('DOMContentLoaded', function () {
-  setInterval(changeBackgroundImage, 5000); // 5000 milisaniye (5 saniye) aralıklarla değiştir
+  setInterval(changeBackgroundImage, 2000); // 5000 milisaniye (5 saniye) aralıklarla değiştir
 });
 
-// apiyi kullandıracak fonksiyon
-function askQuestionToAI(question) {
-  return fetch('/ask_ai', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ question: question }),
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log("Alınan Yanıt: ", data); // Yanıtı konsola yazdır
-    return data;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-}
+const userInput = document.getElementById('userInput');
+userInput.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    sendMessage();
+  }
+});
